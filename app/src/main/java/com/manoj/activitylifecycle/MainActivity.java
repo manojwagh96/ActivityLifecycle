8fb1d49555bd93b955930d453a59ac862610b760
Manoj <waghmanoj96@gmail.com>
2020-11-24 17:07:14 +0530

    Initial commit

---
+++ app/src/main/java/com/manoj/activitylifecycle/MainActivity.java
@@ -0,0 +1,50 @@
+package com.manoj.activitylifecycle;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toast("OnCreate is Called!");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        toast("OnStart is Called!!");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        toast("OnResume is Called!!");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        toast("OnPause is Called!!");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        toast("OnStop is called!!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        toast("OnDestroy is Called!!");
+    }
+
+    public void toast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
